1a2749226b56915fb9f38cbf72e11b88a4f6f16f
“Cristina <coniglicristina@gmail.com>
2024-02-27 13:56:46 +0100

    componentini preparati

---
+++ src/components/delivery/DeliveryCreation.js
@@ -0,0 +1,9 @@
+
+
+//Tale pulsante deve portare ad una pagina dove è possibile 
+//scegliere l'orario di consegna (solo in giornata) a scatti di 15 minuti 
+//(a partire da ora attuale + 2 min per unità di distanza) e si devono poter inserire 
+//delle note (allergie, scala, citofono e simili)
+
+//Una volta confermato l'orario portare ad una pagina dove viene scelto 
+//il pagamento e confermato l'ordine--> deliveryConfirmed
