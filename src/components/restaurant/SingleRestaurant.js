84fce6c3eba4f3f19345b63d35ef582024880124
“Cristina <coniglicristina@gmail.com>
2024-02-28 14:00:29 +0100

    k

--- src/components/restaurant/SingleRestaurant.js
+++ src/components/restaurant/SingleRestaurant.js
@@ -3,9 +3,8 @@ import { Link } from "react-router-dom";
 import { client } from "../../App";
 
 
-export default function SingleRestaurant(props)
-{
-    const [user,setUser] = useAtom(client);
+export default function SingleRestaurant(props) {
+    const [user, setUser] = useAtom(client);
     //vedere tutti i ristoranti (solo nome, immagine, se APERTO, foodTypes e DISTANZA)
 
     return (
@@ -14,13 +13,13 @@ export default function SingleRestaurant(props)
                 <img src={props.r.imgUrl} className="card-img-top" alt="Restaurant image" style={{ width: "100%", height: "100%" }} />
                 <div className="card-body">
                     <h5 className="card-title">{props.r.name}</h5>
-                    <h4 className="card-title">{props.r.open ? "open":"closed"}  </h4>
-                    <h3 className="card-title">{props.r.foodTypes}</h3> 
+                    <h4 className="card-title">{props.r.open ? "open" : "closed"}  </h4>
+                    <h3 className="card-title">{props.r.foodTypes}</h3>
                     <p className="card-text">distance: {props.r.distance}</p>
-                    <Link className="btn btn-primary" to={"/restaurants/"+user.id+"/"+props.r.id} >Detail</Link>
+                    <Link className="btn btn-primary" to={"/restaurants/" + user.id + "/" + props.r.id} >Detail</Link>
                 </div>
             </div>
-            </div>
+        </div>
         </>
     );
 }
