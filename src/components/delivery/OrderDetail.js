4301ef35e5ae80c5e394986f47e5ec2b762544c7
Luca <lucadg27@hotmail.it>
2024-03-06 15:13:26 +0100

    logo zoom

--- src/components/delivery/OrderDetail.js
+++ src/components/delivery/OrderDetail.js
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function OrderDetail({ order }) {
     return (
-        <div className="col-10 container justify-content-between mb-4">
+        <div className="col-12 container justify-content-between mb-4">
             <div className="col">
-                <h5 className="fw-semibold">{order.restaurant_name}</h5>
-                <p><b>Order Notes:</b> {order.note ? order.note : "No notes on this order"}</p>
+                <h5 className="fw-semibold border border-dark rounded mx-4">{order.restaurant_name}</h5>
+                <p><b>Order Notes:</b></p>
+                <p className="px-1">{order.note ? order.note : "No notes on this order"}</p>
             </div>
             <div className="text-start col-auto">
                 <h6><b>Dish Ordered:</b></h6>
